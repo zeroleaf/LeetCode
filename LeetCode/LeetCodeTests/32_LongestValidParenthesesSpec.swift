59b0663b1c52ca7cd5bbd57d0368d3b02332eec2
zeroleaf <zeroleaf021@gmail.com>
2016-02-13 18:46:25 +0800

    Add Longest Valid Parentheses solution using stack.

--- LeetCode/LeetCodeTests/32_LongestValidParenthesesSpec.swift
+++ LeetCode/LeetCodeTests/32_LongestValidParenthesesSpec.swift
@@ -26,17 +26,48 @@ class LongestValidParentheses {
     // 但其实 2.2 覆盖了 2.1 的情况, 因为如果 longest[i - 1] 没有任何匹配, 其值为 0, 
     // longest[i] = longest[i-1] + 2 + longest[i-longest[i-1]-2] = 0 + 2 + longest[i-0-2] = longest[i-2] + 2
     // 如下的算法之所以添加一个前缀是因为开始的 ")" 不会与任何括号元素匹配, 同时避免边界检测.
-    func longestValidParentheses(s: String) -> Int {
-        let input = ")" + s
-        var longest = [Int](count: input.characters.count, repeatedValue: 0)
+//    func longestValidParentheses(s: String) -> Int {
+//        let input = ")" + s
+//        var longest = [Int](count: input.characters.count, repeatedValue: 0)
+//
+//        for var i = 1; i < input.characters.count; i++ {
+//            if input[i] == ")" && input[i - longest[i - 1] - 1] == "(" {
+//                longest[i] = longest[i - 1] + 2 + longest[i - longest[i - 1] - 2]
+//            }
+//        }
+//
+//        return longest.maxElement()!
+//    }
 
-        for var i = 1; i < input.characters.count; i++ {
-            if input[i] == ")" && input[i - longest[i - 1] - 1] == "(" {
-                longest[i] = longest[i - 1] + 2 + longest[i - longest[i - 1] - 2]
+    // Inspired by https://leetcode.com/discuss/7609/my-o-n-solution-using-a-stack
+
+    // st 中保存的是未匹配的括号的索引位置, 因此最后的 while 循环是找出位置为 a 处的有效括号匹配长度
+    // 其中的最大值即为最长的有效括号匹配长度.
+    func longestValidParentheses(s: String) -> Int {
+
+        let n = s.characters.count
+
+        var st = [Int]()
+        for var i = 0; i < n; i++ {
+            if s[i] == ")" && !st.isEmpty && s[st.last!] == "(" {
+                st.removeLast()
+            } else {
+                st.append(i)
             }
         }
 
-        return longest.maxElement()!
+        if st.isEmpty {
+            return n
+        }
+
+        var a = n, b = 0, longest = 0
+        while !st.isEmpty {
+            b = st.removeLast()
+            longest = max(longest, a - b - 1)
+            a = b
+        }
+        longest = max(longest, a)
+        return longest
     }
 }
 
